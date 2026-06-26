df6c3b70c2445558584e0ab9280b4aa611e794ef
Jinyeong Choi <choiwlsd@gmail.com>
2026-06-26 22:27:53 +0900

    Fix: useInfoDetail function name

--- src/features/info-sharing/hooks/useInfoDetail.ts
+++ src/features/info-sharing/hooks/useInfoDetail.ts
@@ -3,7 +3,7 @@ import { type InfoPostDetail } from "../types/info-sharing.type"
 import { getInfoDetailById } from "../api/info-sharing.api";
 
 // 정보 공유글 상세 조회
-export const useInfos = (id: number) => {
+export const useInfoDetail = (id: number) => {
     const [data, setData] = useState<InfoPostDetail | null>(null);
 
     useEffect(() => {
