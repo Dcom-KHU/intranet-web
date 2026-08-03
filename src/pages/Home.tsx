ebbbaaefbea30291ebcef1a70e0835fa10d948ec
Jinyeong Choi <choiwlsd@gmail.com>
2026-08-04 06:31:14 +0900

    Chore: 홈 화면 순서 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,6 +34,39 @@ const Home = () => {
 
                 <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
 
+                    <Container title="족보" icon={IoPencilOutline} onViewAllClick={() => navigate("/exam-archive")}>
+                        {exam?.slice(0, 5).map(item => (
+                            <div
+                                key={item.id}
+                                className="flex py-1 text-sm hover:font-bold cursor-pointer items-center justify-between overflow-hidden"
+                                onClick={() => navigate(`/exam-archive/${item.id}`)}
+                            >
+                                <p className="min-w-0 truncate">
+                                    {item.author.name}
+                                    {" | "}
+                                    
+                                    {item.professor}
+                                    {" - "}
+                                    <span className="inline">
+                                    {item.subject.length > 8
+                                        ? `${item.subject.slice(0, 8)}...`
+                                        : item.subject}
+                                    </span>
+                                </p>
+
+                                <p className="text-gray-400 text-xs flex-shrink-0 ml-2">
+                                    <span className="sm:hidden">
+                                        {formatMobileHomeDate(item.date)}
+                                    </span>
+
+                                    <span className="hidden sm:inline">
+                                        {formatHomeDate(item.date)}
+                                    </span>
+                                </p>
+                            </div>
+                        ))}
+                    </Container>
+
                     <Container title="공지사항" icon={IoNotificationsOutline} onViewAllClick={() => navigate("/notice")}>
                         {notices?.slice(0, 5).map(item => (
                             <div
@@ -90,38 +123,8 @@ const Home = () => {
                         ))}
                     </Container>       
                     
-                    <Container title="족보" icon={IoPencilOutline} onViewAllClick={() => navigate("/exam-archive")}>
-                        {exam?.slice(0, 5).map(item => (
-                            <div
-                                key={item.id}
-                                className="flex py-1 text-sm hover:font-bold cursor-pointer items-center justify-between overflow-hidden"
-                                onClick={() => navigate(`/exam-archive/${item.id}`)}
-                            >
-                                <p className="min-w-0 truncate">
-                                    {item.author.name}
-                                    {" | "}
-                                    
-                                    {item.professor}
-                                    {" - "}
-                                    <span className="inline">
-                                    {item.subject.length > 8
-                                        ? `${item.subject.slice(0, 8)}...`
-                                        : item.subject}
-                                    </span>
-                                </p>
+                    
 
-                                <p className="text-gray-400 text-xs flex-shrink-0 ml-2">
-                                    <span className="sm:hidden">
-                                        {formatMobileHomeDate(item.date)}
-                                    </span>
-
-                                    <span className="hidden sm:inline">
-                                        {formatHomeDate(item.date)}
-                                    </span>
-                                </p>
-                            </div>
-                        ))}
-                    </Container>
                     <div className="lg:col-span-3 [&>div]:h-full">
                         <Container title="활동사진" icon={IoImageOutline} onViewAllClick={() => navigate("/gallery")}>
                             <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
